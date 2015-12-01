7c7cd102c5860d54a9aa80c09a90eb6a2a2f50e4
Joe Winter <joe@joeventures.com>
2015-12-01 12:29:55 -0500

    Begin actors controller

---
+++ app/controllers/actors_controller.rb
@@ -0,0 +1,20 @@
+class ActorsController < ApplicationController
+
+  before_action :authenticate_user!
+
+  def create
+    #todo
+  end
+
+  def update
+    #todo
+  end
+
+  def show
+    #todo
+  end
+
+  def delete
+    #todo
+  end
+end
