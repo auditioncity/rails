0146b16f9d24b5771d9f567fcd82331a3fdc032c
Joe Winter <joe@joeventures.com>
2015-12-02 16:01:01 -0500

    Actors controller - Create

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -3,7 +3,25 @@ class ActorsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    #todo
+    user = current_user
+    @actor = user.actors.new(bio: params[:bio],
+                             age_young: params[:age_young],
+                             age_old: params[:age_old],
+                             height_feet: params[:height_feet],
+                             height_inches: params[:height_inches],
+                             hair_color: params[:hair_color],
+                             eye_color: params[:eye_color],
+                             skills: params[:skills],
+                             gender: params[:gender],
+                             ethnicity: params[:ethnicity],
+                             talent_agency: params[:talent_agency],
+                             union: params[:union])
+    if @actor.save
+      render "create", status: :created
+    else
+      render json: {errors: @actor.errors.full_messages },
+             status: :unprocessable_entity
+    end
   end
 
   def update
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
   post "/signup", to: "registrations#create"
   delete "/signup", to: "registrations#destroy"
   post "/login", to: "registrations#login"
+
+  # ACTOR ROUTES
+  post "/actors/new", to: "actors#create"
+  put "/actors/:id", to: "actors#update"
+  get "/actors/:id", to: "actors#show"
+  get "/actors", to: "actors#index"
+  delete "/actors/:id", to: "actors#destroy"
+
 end
