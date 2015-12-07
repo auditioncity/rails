ae79dd0c75431fdd2219d7da3081eed6e208111f
Joe Winter <joe@joeventures.com>
2015-12-07 10:45:19 -0500

    More paperclip stuff. Add headshot sizes and correct some credentials.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -5,6 +5,12 @@ class Actor < ActiveRecord::Base
   has_many :phone_numbers, as: :contactable, class_name: "PhoneNumber", dependent: :destroy
   has_many :websites, as: :contactable, class_name: "Website", dependent: :destroy
 
-  has_attached_file :headshot
-  validates_attachment :headshot, content_type: { content_type: "image/jpeg"}, size: {in: 0..5.megabytes}
+  has_attached_file :headshot, styles: {
+      thumb: '124x155>',
+      small: '400x500>',
+      mobile: '800x1000>',
+      large: '1600x2000>',
+      full: '2400x3000>'
+  }
+  validates_attachment :headshot, content_type: { content_type: "image/jpeg"}, size: {in: 0..4.megabytes}
 end
