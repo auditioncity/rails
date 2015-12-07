a5a065c6a97d6cf419c98e00eace181d92839201
Joe Winter <joe@joeventures.com>
2015-12-07 16:15:41 -0500

    Change requested by Front End for create user controller

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -4,29 +4,28 @@ class ActorsController < ApplicationController
 
   def create
     user = current_user
-    @actor = user.actors.new(bio: params[:bio],
-                             age_young: params[:age_young],
-                             age_old: params[:age_old],
-                             height_feet: params[:height_feet],
-                             height_inches: params[:height_inches],
-                             hair_color: params[:hair_color],
-                             eye_color: params[:eye_color],
-                             skills: params[:skills],
-                             gender: params[:gender],
-                             ethnicity: params[:ethnicity],
-                             talent_agency: params[:talent_agency],
-                             union: params[:union],
-                             headshot: params[:headshot],
-                             resume: params[:resume])
-    @actor.addresses.new(address1: params[:address1],
-                         address2: params[:address2],
-                         city: params[:city],
-                         state: params[:state],
-                         zip: params[:zip])
-    @actor.emails.new(label: params[:email_type], info: params[:email])
-    @actor.phone_numbers.new( label: params[:phone_type], info: params[:phone_number])
-    @actor.websites.new( label: params[:website_type], info: params[:website_url])
-    #binding.pry
+    @actor = user.actors.new(bio: params["info.bio"],
+                             age_young: params["info.age_young"],
+                             age_old: params["info.age_old"],
+                             height_feet: params["info.height_feet"],
+                             height_inches: params["info.height_inches"],
+                             hair_color: params["info.hair_color"],
+                             eye_color: params["info.eye_color"],
+                             skills: params["info.skills"],
+                             gender: params["info.gender"],
+                             ethnicity: params["info.ethnicity"],
+                             talent_agency: params["info.talent_agency"],
+                             union: params["info.union"],
+                             headshot: params["info.headshot"],
+                             resume: params["info.resume"])
+    @actor.addresses.new(address1: params["info.address1"],
+                         address2: params["info.address2"],
+                         city: params["info.city"],
+                         state: params["info.state"],
+                         zip: params["info.zip"])
+    @actor.emails.new(label: params["info.email_type"], info: params["info.email"])
+    @actor.phone_numbers.new( label: params["info.phone_type"], info: params["info.phone_number"])
+    @actor.websites.new( label: params["info.website_type"], info: params["info.website_url"])
 
     if @actor.save
       render "show.json.jbuilder", status: :created
