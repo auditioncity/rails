e00d1e490ba8de5b6d6ce9946ead74efdfd47533
Joe Winter <joe@joeventures.com>
2015-12-09 15:07:13 -0500

    Add actor contact routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,14 @@ Rails.application.routes.draw do
   get "/actors", to: "actors#index"
   delete "/actors/:id", to: "actors#destroy"
 
+  # CONTACT INFO
+  post "actors/:actor_id/address", to: "actors#add_address"
+  put "/actors/:actor_id/address/:type", to: "actors#update_address"
+  post "/actors/:actor_id/email", to: "actors#add_email"
+  put "/actors/:actor_id/email/:type", to: "actors#update_email"
+  post "/actors/:actor_id/phone", to: "actors#add_phone"
+  put "/actors/:actor_id/phone/:type", to: "actors#update_phone"
+  post "/actors/:actor_id/url", to: "actors#add_url"
+  put "/actors/:actor_id/url/:type", to: "actors#update_url"
+
 end
