5d4b8ad4485f4c6210d9c3383000c2b87bc6179c
Joe Winter <joe@joeventures.com>
2015-12-10 18:01:04 -0500

    Update actors controller and views to add/update/delete contact info

--- config/routes.rb
+++ config/routes.rb
@@ -13,12 +13,16 @@ Rails.application.routes.draw do
 
   # CONTACT INFO
   post "/actors/:actor_id/address", to: "actors#add_address"
-  put "/actors/:actor_id/address/:type", to: "actors#update_address"
+  put "/actors/:actor_id/address/:old_type", to: "actors#update_address"
+  delete "/actors/:actor_id/address/:type", to: "actors#delete_address"
   post "/actors/:actor_id/email", to: "actors#add_email"
-  put "/actors/:actor_id/email/:type", to: "actors#update_email"
+  put "/actors/:actor_id/email/:old_type", to: "actors#update_email"
+  delete "/actors/:actor_id/email/:type", to: "actors#delete_email"
   post "/actors/:actor_id/phone", to: "actors#add_phone"
-  put "/actors/:actor_id/phone/:type", to: "actors#update_phone"
+  put "/actors/:actor_id/phone/:old_type", to: "actors#update_phone"
+  delete "/actors/:actor_id/phone/:type", to: "actors#delete_phone"
   post "/actors/:actor_id/url", to: "actors#add_url"
-  put "/actors/:actor_id/url/:type", to: "actors#update_url"
+  put "/actors/:actor_id/url/:old_type", to: "actors#update_url"
+  delete "/actors/:actor_id/url/:type", to: "actors#delete_url"
 
 end
