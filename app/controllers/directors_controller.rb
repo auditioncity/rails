e772eaeab5143ee9b1db7ce362ecd2c843fe022b
Joe Winter <joe@joeventures.com>
2015-12-12 10:50:53 -0500

    Set up directors controller

---
+++ app/controllers/directors_controller.rb
@@ -0,0 +1,24 @@
+class DirectorsController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    #todo
+  end
+
+  def update
+    #todo
+  end
+
+  def show
+    #todo
+  end
+
+  def index
+    #todo
+  end
+
+  def destroy
+    #todo
+  end
+
+end
