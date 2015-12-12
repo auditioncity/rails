0482b30f84e27f8f48131d82ccc7df95e7ddadc0
Joe Winter <joe@joeventures.com>
2015-12-12 10:46:06 -0500

    Set up director model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   has_secure_password
 
   has_many :actors, dependent: :destroy
+  has_many :directors, dependent: destroy
 
   before_validation :ensure_access_token!
 
