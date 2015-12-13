46cdfdf4aebcf5ad6371c159027684291000df0d
Joe Winter <joe@joeventures.com>
2015-12-13 08:13:29 -0500

    Start the decision controller

---
+++ app/controllers/decisions_controller.rb
@@ -0,0 +1,24 @@
+class DecisionsController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    #todo
+  end
+
+  def update
+    #todo
+  end
+
+  def show
+    #todo
+  end
+
+  def index
+    #todo
+  end
+
+  def destroy
+    #todo
+  end
+
+end
