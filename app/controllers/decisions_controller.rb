5edf976ab313970a2d66c75b1aa425d6da3ad263
Joe Winter <joe@joeventures.com>
2015-12-13 11:23:57 -0500

    Decisions Controller

--- app/controllers/decisions_controller.rb
+++ app/controllers/decisions_controller.rb
@@ -2,23 +2,57 @@ class DecisionsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    #todo
+    @decision = current_user.director.decisions.new
+    @decision.actor_id = params[:actor_id]
+    @decision.notes = params[:notes]
+    @decision.callback = params[:callback] == "true" ? true : false
+    if @decision.save
+      render "show.json.jbuilder", status: :created
+    else
+      render json: {errors: @decision.errors.full_messages},
+             status: :unprocessable_entity
+    end
   end
 
   def update
-    #todo
+    @decision = Decision.find(params[:id])
+    @decision.notes = params[:notes]
+    @decision.callback = params[:callback] == "true" ? true : false
+    if @decision.save
+      render "update.json.jbuilder"
+    else
+      render json: {errors: @decision.errors.full_messages},
+             status: :unprocessable_entity
+    end
   end
 
   def show
-    #todo
+    @decision = Decision.find(params[:id])
+    if @decision
+      render "show.json.jbuilder", status: :ok
+    else
+      render json: {errors: @decision.errors.full_messages},
+             status: :unprocessable_entity
+    end
   end
 
   def index
-    #todo
+    @decisions = Decision.find_by_director_id(current_user.director)
+    if @decisions
+      render "index.json.jbuilder"
+    else
+      render json: {errors: @decisions.errors.full_messages},
+             status: :unprocessable_entity
+    end
   end
 
   def destroy
-    #todo
+    if Decision.find(params[:id]).destroy
+      render json: {success: "Decision Deleted"}, status: :ok
+    else
+      render json: {error: "That Decision does not exist."},
+             status: :not_found
+    end
   end
 
 end
